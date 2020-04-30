7d5c46d9dfb00e18f15871efd5fd84447455ec66
Glitch (hexagonal-potent-bush) <none>
2020-04-30 21:28:59 +0000

    🐬💆 Checkpoint ./server.js:15407664/371

--- server.js
+++ server.js
@@ -30,7 +30,13 @@ var urlSchema = new Schema({
    
   });
 
-var ShortURL = mongoose.model('shortUrl', urlSchema);
+var shortURL = mongoose.model('shortUrl', urlSchema);
+//with this video to get url https://www.youtube.com/watch?v=5T1YDRWaa3k
+app.get("/new/:urlToShort(*)",function(req,res,next){
+        var {urlToShort}=req.params;  
+  return res.json({urlToShor})
+        })
+
 
 
 
